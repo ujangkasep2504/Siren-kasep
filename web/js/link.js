8952d078fb1695e08c511a27d3d367ad0e6e8bd1
budakbaong02 <lagukulagumu02@gmail.com>
2025-05-30 05:37:59 +0700

    Update link.js

--- web/js/link.js
+++ web/js/link.js
@@ -12,12 +12,11 @@ let selectedProxy = null
 const defaultProxyUrl = "https://raw.githubusercontent.com/AFRcloud/ProxyList/refs/heads/main/ProxyList.txt"
 
 const serverDomains = [
-  { value: "", label: "Default" },
-  { value: "pop.budakbaong.my.id", label: "pop.budakbaong.my.id" },
-  { value: "a.budakbaong.my.id", label: "a.budakbaong.my.id" },
-  { value: "b.budakbaong.my.id", label: "b.budakbaong.my.id" },
-  { value: "c.budakbaong.my.id", label: "c.budakbaong.my.id" },
-  { value: "manual", label: "Type Manual" },
+  "pop.budakbaong.my.id",
+  "a.budakbaong.my.id",
+  "b.budakbaong.my.id",
+  "c.budakbaong.my.id",
+  "manual"
 ]
 let selectedServerDomain = serverDomains[5] // Default to first domain
 const defaultUUID = "80b640ff-031d-4294-8b9d-12a41d976444"
